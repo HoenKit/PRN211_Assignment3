637675d906550a7ee25842c5e7dbadc882b93683
HoenKit <hoangkaito03@gmail.com>
2024-03-24 22:27:30 +0700

    fix detail product

--- PRN211_Assignment3Web/obj/Debug/net7.0/PRN211_Assignment3Web.AssemblyInfo.cs
+++ PRN211_Assignment3Web/obj/Debug/net7.0/PRN211_Assignment3Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRN211_Assignment3Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96793a1c5b23809ab6bd08cc2f469d2e9ac6e863")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd685f2bde51e96d098c0ce439b8a7b1617a0f21")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRN211_Assignment3Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRN211_Assignment3Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
